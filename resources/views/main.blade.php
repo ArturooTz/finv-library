cababdb1a468b46299e5d1249235eed89c3c0c15
arturootz <jtz307@hotmail.com>
2018-10-08 15:59:45 -0600

    Diseño inicial

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,37 +4,77 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+		<link rel="stylesheet" href="css/styles.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
         <title>FINV Library 1.0</title>
 
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+        <div class="flex-center">
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+				<h1>Test</h1>
+                <div class="items-container">
+					<div class="item">
+						<a href="#" class="card-link">
+							<div class="card-wrap">
+								<div class="info-element">
+									<span>URL: www.testsite.com</span>
+									<span>Units: 1, 2, 4, 5</span>
+									<span>Preview</span>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="item">
+						<a href="#" class="card-link">
+							<div class="card-wrap">
+								<div class="info-element">
+									<span>URL: www.testsite.com</span>
+									<span>Units: 1, 2, 4, 5</span>
+									<span>Preview</span>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="item">
+						<a href="#" class="card-link">
+							<div class="card-wrap">
+								<div class="info-element">
+									<span>URL: www.testsite.com</span>
+									<span>Units: 1, 2, 4, 5</span>
+									<span>Preview</span>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="item">
+						<a href="#" class="card-link">
+							<div class="card-wrap">
+								<div class="info-element">
+									<span>URL: www.testsite.com</span>
+									<span>Units: 1, 2, 4, 5</span>
+									<span>Preview</span>
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+	<script>
+	
+		$(".card-link").click(function(){
+			//$(this).css("transform","scale(0.95)");
+			//myVar = setTimeout(myFunc(), 400);
+			//clearTimeout(myVar);
+		});
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+		function myFunc(){
+			$(this).css("transform","" );
+		}
+	</script>
 </html>
